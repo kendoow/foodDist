54b2aceaf8f3da4720bcdeb6305ee44f5e6da826
kendo <kendo_ow@mail.ru>
2021-02-25 19:26:55 +0300

    done

---
+++ Food_dist/js/script.js
@@ -0,0 +1,30 @@
+  
+import tabs from './Modules/tabs';
+import modal from './Modules/modal';
+import timer from './Modules/timer';
+import cards from './Modules/cards';
+import calc from './Modules/calc';
+import forms from './Modules/forms';
+import slider from './Modules/slider';
+import {openModal} from './Modules/modal';
+
+window.addEventListener('DOMContentLoaded', function() {
+    const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 50000);
+
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+    modal('[data-modal]', '.modal', modalTimerId);
+    timer('.timer', '2021-06-11');
+    cards();
+    calc();
+    forms('form', modalTimerId);
+    slider({
+        container: '.offer__slider',
+        slide: '.offer__slide',
+        nextArrow: '.offer__slider-next',
+        prevArrow: '.offer__slider-prev',
+        totalCounter: '#total',
+        currentCounter: '#current',
+        wrapper: '.offer__slider-wrapper',
+        field: '.offer__slider-inner'
+    });
+});
